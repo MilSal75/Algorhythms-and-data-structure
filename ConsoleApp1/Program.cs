b8fb46c63929d34006a4ffb2fb5cc1e2fb404f13
MilSal75 <m.salyakhiyeva@sibstrin.ru>
2025-02-23 22:30:59 +0700

    игра GuessNumber

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 101);
+            while (true)
+            {
+                Console.WriteLine("Enter your try in [1,100]");
+                int tries = 0;
+
+                while (!int.TryParse(Console.ReadLine(), out tries))
+                    Console.WriteLine("Error. Enter correct number in [1,100]");
+                if (tries > number)
+                    Console.WriteLine("Your try is more");
+                else if (tries < number)
+                    Console.WriteLine("Your try is less");
+                else
+                {
+                    Console.WriteLine("You win");
+                    break;
+                }
+            }
+        }
+    }
+}
